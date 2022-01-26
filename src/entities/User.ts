06892a5bf5440ca781588023c2e373618d2bacd1
Alan Cavalcante <alanlimaemail@gmail.com>
2022-01-26 15:19:09 -0300

    create User Entity

---
+++ src/entities/User.ts
@@ -0,0 +1,29 @@
+import { Column, PrimaryColumn, CreateDateColumn, UpdateDateColumn} from "typeorm";
+import{ v4 as uuid} from "uuid";
+
+export default class User {
+  @PrimaryColumn()
+  public id: string;
+
+  @Column()
+  public name: string;
+
+  @Column()
+  public email: string;
+
+  @Column()
+  public password: string;
+
+  @CreateDateColumn()
+  public createdAt: Date;
+
+  @UpdateDateColumn()
+  public updatedAt: Date;
+
+  constructor(){
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+
+}
