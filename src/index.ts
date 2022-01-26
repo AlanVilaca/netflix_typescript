fe08c0e07f475855f3a5b5e0a6a9ffd8175315ef
Alan Cavalcante <alanlimaemail@gmail.com>
2022-01-26 12:07:39 -0300

    config eslint

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,8 @@ import express from "express";
 import cors from "cors";
 const app = express();
 
-console.log("Ola")
-
 app.use(cors());
 
 app.listen(3000, ()=>{
-    console.log("listening on port 3000")
-})
+  console.log("listening on port 3000");
+});
