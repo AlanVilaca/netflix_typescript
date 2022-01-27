f8475329535da122916df82364b0c3ea2c4d8548
Alan Cavalcante <alanlimaemail@gmail.com>
2022-01-27 12:06:38 -0300

    create hashPassword function

--- src/helper/user.helper.ts
+++ src/helper/user.helper.ts
@@ -1,3 +1,5 @@
+import bcrypt from "bcryptjs";
+
 export function validateEmail(email: string): boolean {
   const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return regex.test(String(email).toLowerCase());
@@ -7,4 +9,10 @@ export function validatePassword(password: string): boolean {
   if (password.length < 5) return false;
 
   return true;
+}
+
+export async function hashPassword(password: string) : Promise<string> {
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+  return hash;
 }
