db2d824016c9a21a2c75c4ba726a43f59fad228f
Alan Cavalcante <alanlimaemail@gmail.com>
2022-01-27 12:08:38 -0300

    create SessionController

---
+++ src/controller/SessionController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import CreateSession from "../services/Session/CreateSession";
+
+export default class SessionController {
+  async login(req:Request, res: Response){
+    const {email, password} = req.body;
+
+    const service = new CreateSession();
+    const result = await service.execute({email, password});
+    if (result instanceof Error) {
+      return res.status(400).json(result.message);
+    }
+
+    return res.json(result);
+  }
+}
