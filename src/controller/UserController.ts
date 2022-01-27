851886217b005e71d7f0dcd5574f3237fe496787
Alan Cavalcante <alanlimaemail@gmail.com>
2022-01-27 12:08:20 -0300

    change name to UserController

---
+++ src/controller/UserController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import { validateEmail, validatePassword } from "../helper/user.helper";
+import CreateUser from "../services/User/CreateUser";
+
+export default class User {
+  async sign_up(req:Request, res: Response) {
+    const {name, email, password, passwordConfirmation} = req.body;
+
+    if (password !== passwordConfirmation) return res.status(400).json("Password and password confirmation are different");
+    if (!validateEmail(email)) return res.status(400).json("Email bad formatted");
+    if (!validatePassword(password)) return res.status(400).json("Password weak");
+
+    const service = new CreateUser();
+    const result = await service.execute({name, email, password});
+    if (result instanceof Error) {
+      return res.status(400).json(result.message);
+    }
+
+    return res.json(result);
+  }
+}
