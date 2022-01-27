b5a287bed5f20580d213b6c6e3e4bc109d09f036
Alan Cavalcante <alanlimaemail@gmail.com>
2022-01-27 08:27:14 -0300

    create user helper

---
+++ src/helper/user.helper.ts
@@ -0,0 +1,10 @@
+export function validateEmail(email: string): boolean {
+  const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regex.test(String(email).toLowerCase());
+}
+
+export function validatePassword(password: string): boolean {
+  if (password.length < 5) return false;
+
+  return true;
+}
