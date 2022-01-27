cee43fbc4e2c486ce1c86758a68baa5d269160ec
Alan Cavalcante <alanlimaemail@gmail.com>
2022-01-27 08:24:27 -0300

    add express json and urlencoded

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,11 @@ import "./database";
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+
+import generalRoutes from "./routes/generalRoutes";
+generalRoutes(app);
 
 app.listen(3000, ()=>{
   console.log("listening on port 3000");
