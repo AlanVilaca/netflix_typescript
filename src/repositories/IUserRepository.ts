01a426d91e13ae52c86ba6564f9525ad0437f0da
Alan Cavalcante <alanlimaemail@gmail.com>
2022-02-02 08:14:42 -0300

    create userRepositories

---
+++ src/repositories/IUserRepository.ts
@@ -0,0 +1,11 @@
+import { ICreateUser } from "../interface/user/ICreateUser";
+import { IUser } from "../interface/user/IUser";
+
+interface IUserRepository {
+  create(user: ICreateUser): Promise<IUser>;
+  save(user: IUser): Promise<IUser>;
+  findById(id: string): Promise<IUser | undefined>;
+  findByEmail(email: string): Promise<IUser | undefined>;
+}
+
+export default IUserRepository;
