2703ea1f0be859bf0735ef8a5a9fae99a738dd07
Alan Cavalcante <alanlimaemail@gmail.com>
2022-02-02 08:25:05 -0300

    add space to better read

--- ormconfig.ts
+++ ormconfig.ts
@@ -5,11 +5,11 @@ const rootDir = process.env.NODE_ENV === "production" ? "dist" : "src";
 
 export default {
   "type": process.env.DB_DIALECT,
-  "host":process.env.DB_HOST,
-  "port":process.env.DB_PORT,
-  "username":process.env.DB_USER,
-  "password":process.env.DB_PASSWORD,
-  "database":process.env.DB_NAME,
+  "host": process.env.DB_HOST,
+  "port": process.env.DB_PORT,
+  "username": process.env.DB_USER,
+  "password": process.env.DB_PASSWORD,
+  "database": process.env.DB_NAME,
   "entities": [
     `${rootDir}/entities/*.ts`
   ],
