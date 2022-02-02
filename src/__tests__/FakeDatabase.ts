7ba5c517efef5ec75edbc5db79e7314b8e0d8d6d
Alan Cavalcante <alanlimaemail@gmail.com>
2022-02-02 08:23:20 -0300

    create a fakedatabase to test jest

---
+++ src/__tests__/FakeDatabase.ts
@@ -0,0 +1,36 @@
+import User from "../entities/User";
+import { v4 as uuidv4 } from "uuid";
+import IUserRepository from "../repositories/IUserRepository";
+import { ICreateUser } from "../interface/user/ICreateUser";
+
+class FakeUserDatabase implements Omit<IUserRepository, "save"> {
+  private users: User[] = [];
+
+  public async create({ name, email }: ICreateUser): Promise<User> {
+    const user = new User();
+
+    user.id = uuidv4();
+    user.name = name;
+    user.email = email;
+
+    this.users.push(user);
+
+    return user;
+  }
+
+  public async save(user: User): Promise<User> {
+    return user;
+  }
+
+  public async findById(id: string): Promise<User | undefined> {
+    const user = this.users.find(user => user.id === id);
+    return user;
+  }
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    const user = this.users.find(user => user.email === email);
+    return user;
+  }
+}
+
+export default FakeUserDatabase;
