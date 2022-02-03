d1545be3dac2bb5ad96c12f59dd5a9ea6bc8f068
Alan Cavalcante <alanlimaemail@gmail.com>
2022-02-03 16:47:58 -0300

    add in UserController function to unActiveAccount

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from "express";
 import { validateEmail, validatePassword } from "../helper/user.helper";
 import CreateUser from "../services/User/CreateUser";
+import UnActiveAccount from "../services/User/UnActiveAccount";
 
 export default class User {
-  async sign_up(req:Request, res: Response) {
-    const {name, email, password, passwordConfirmation} = req.body;
+  async create(req:Request, res: Response) {
+    const { name, email, password, passwordConfirmation } = req.body;
 
     if (password !== passwordConfirmation) return res.status(400).json("Password and password confirmation are different");
     if (!validateEmail(email)) return res.status(400).json("Email bad formatted");
@@ -12,11 +13,22 @@ export default class User {
 
     const service = new CreateUser();
 
-    const result = await service.execute({name, email, password});
+    const result = await service.execute({ name, email, password });
     if (result instanceof Error) {
       return res.status(400).json({err: result.message});
     }
 
     return res.json(result);
+  }
+
+  async unActiveAccount(req: Request, res: Response) {
+    const id = req.user.id;
+    const service = new UnActiveAccount();
+    const result = await service.execute(id);
+    if (result instanceof Error) {
+      return res.status(400).json({err: result.message});
+    }
+
+    return res.sendStatus(200);
   }
 }
