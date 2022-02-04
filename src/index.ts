98636cfc129b10c349c9486c3cbf2545f66c01bc
Alan Cavalcante <alanlimaemail@gmail.com>
2022-02-04 10:05:59 -0300

    add AppErrror in index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,47 @@ dotenv.config({
   path: process.env.NODE_ENV === "test" ? ".env.test" : ".env"
 });
 
-import express from "express";
-import cors from "cors";
 import "reflect-metadata";
-import "./database";
+import express, { Request, Response } from "express";
+import cors from "cors";
 const app = express();
 
+// connection to database
+import "./database";
+
+// enable cors
 app.use(cors());
+
+// permit read json
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// load routes
 import generalRoutes from "./routes/generalRoutes";
 generalRoutes(app);
 
+
+// load error in requests
+import AppError from "./errors/AppError";
+app.use(
+  (error: Error, request: Request, response: Response) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: "error",
+        message: error.message,
+      });
+    }
+
+    console.log(error);
+
+    return response.status(500).json({
+      status: "error",
+      message: "Internal server error",
+    });
+  },
+);
+
+// create port
 app.listen(process.env.PORT, () => {
   console.log(`listening on port ${process.env.PORT}`);
 });
