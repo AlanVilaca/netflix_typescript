6b203d4469e8d9a539c5c5dcdfb043e5558adc96
Alan Cavalcante <alanlimaemail@gmail.com>
2022-02-09 16:47:56 -0300

    create ProfileEntity

---
+++ src/entities/Profile.ts
@@ -0,0 +1,27 @@
+import {Entity, Column, PrimaryColumn, CreateDateColumn, UpdateDateColumn} from "typeorm";
+import{ v4 as uuid} from "uuid";
+
+@Entity("profiles")
+export default class User {
+  @PrimaryColumn()
+  public id: string;
+
+  @Column()
+  public name: string;
+
+  @Column()
+  public avatar: string;
+
+  @CreateDateColumn()
+  public createdAt: Date;
+
+  @UpdateDateColumn()
+  public updatedAt: Date;
+
+  constructor(){
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+
+}
