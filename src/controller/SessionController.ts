81e2e351717b061a0f98689f5d1edadc5047283c
Alan Cavalcante <alanlimaemail@gmail.com>
2022-02-11 15:31:47 -0300

    add instanceToInstance in return controllers

--- src/controller/SessionController.ts
+++ src/controller/SessionController.ts
@@ -1,14 +1,15 @@
 import { Request, Response } from "express";
 import CreateSession from "../services/Session/CreateSession";
+import { instanceToInstance } from "class-transformer";
 
 export default class SessionController {
   async login(req:Request, res: Response){
-    const {email, password} = req.body;
+    const { email, password } = req.body;
 
-    const service = new CreateSession();
+    const createSessionService = new CreateSession();
 
-    const result = await service.execute({ email, password });
+    const session = await createSessionService.execute({ email, password });
 
-    return res.json(result);
+    return res.json(instanceToInstance(session));
   }
 }
--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -3,6 +3,7 @@ import AppError from "../errors/AppError";
 import { validateEmail, validatePassword } from "../helper/user.helper";
 import CreateUser from "../services/User/CreateUser";
 import UnActiveAccount from "../services/User/UnActiveAccount";
+import { instanceToInstance } from "class-transformer";
 
 export default class User {
   async create(req:Request, res: Response): Promise<Response> {
@@ -12,16 +13,16 @@ export default class User {
     if (!validateEmail(email)) throw new AppError("Email bad formatted");
     if (!validatePassword(password)) throw new AppError("Password weak");
 
-    const service = new CreateUser();
+    const createUserService = new CreateUser();
 
-    const result = await service.execute({ name, email, password });
-    return res.json(result);
+    const user = await createUserService.execute({ name, email, password });
+    return res.json(instanceToInstance(user));
   }
 
   async unActiveAccount(req: Request, res: Response) {
     const id = req.user.id;
-    const service = new UnActiveAccount();
-    await service.execute(id);
+    const unActiveAccountService = new UnActiveAccount();
+    await unActiveAccountService.execute(id);
 
     return res.sendStatus(200);
   }
